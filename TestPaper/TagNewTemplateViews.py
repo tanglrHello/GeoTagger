e3344d5a6f569ad614c25bedf18a53b003220749
tanglr <tanglr@tanglrdeMacBook-Pro.local>
2016-10-16 14:16:56 +0800

    change second template

--- TestPaper/TagNewTemplateViews.py
+++ TestPaper/TagNewTemplateViews.py
@@ -61,12 +61,12 @@ def tagNewTemplate(request):
         fullQuestion = result[6]
         if papertype == "choice":
             # 将text拆成题面和选项，供前端显示
+            print textInfo['text']
             textInfo['timian'] = textInfo['text'].split("\t")[0]
             textInfo['xuanxiang'] = textInfo['text'].split("\t")[1]
             textInfo['combinedTextWithoutTab'] = textInfo['text'].replace("\t", "")
 
     username = request.COOKIES.get("username", "")
-    print "masld;kfjlej",username
 
     tagtype = ""
     if request.method == 'GET':
